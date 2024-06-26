bc06357813742137855e5bdc476d533e3c3326c6
Pavan Rayudu <122986277+pavanRayudu@users.noreply.github.com>
2024-06-26 07:26:48 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
   const [showAddMoneyModal, setShowAddMoneyModal] = useState(false);
 
   function getDayState() {
-    let state = "Morning";
+    let state = "Good Morning";
        const now = new Date().getHours();
        if(now >= 12 && now < 16) {
-         state = "Afternoon";
+         state = "Good Afternoon";
        } else if(now > 16 && now < 24) {
-         state = "Evening";
+         state = "Good Evening";
        } else if(now > 22 || now< 5) {
          state = "Please sleep 🙏";
        }
@@ -22,7 +22,7 @@ const Header = () => {
   
   return (
     <div className='header'>
-      <h1 id='greeting'>Good {getDayState()}, Venkat!</h1>
+      <h1 id='greeting'>{getDayState()}, Venkat!</h1>
       <div className='button-group'>
         <button id='add-money-btn' onClick={() => setShowAddMoneyModal(true)}>Add Money</button>
         <button id='add-expense-btn' onClick={() => setShowExpenseModal(true)}>Add Expense</button>
