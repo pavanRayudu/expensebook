ee7e94252ddfc596c423dcf04b5d2a6d55799075
Pavan Rayudu <122986277+pavanRayudu@users.noreply.github.com>
2024-06-30 08:36:53 +0530

    Update ExpenseContext.jsx

--- src/components/context/ExpenseContext.jsx
+++ src/components/context/ExpenseContext.jsx
@@ -81,12 +81,6 @@ export function ExpenseContextProvider({ children }) {
         console.log(filters)
         let filterByCategory = [];
         let filterByMonth = [];
-
-        if(filters.expenseType === "Category" && filters.expenseMonth === "month"){
-            filterByCategory = [...expenseList];
-            setExpenseList2(filterByCategory)
-            return;
-        }
         
         if (filters.expenseType === "Category") {
             filterByCategory = [...expenseList];
