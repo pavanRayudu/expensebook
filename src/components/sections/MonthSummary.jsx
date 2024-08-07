38e0146f5f3a65346822802fce96fbdf9dc73531
Pavan Rayudu <122986277+pavanRayudu@users.noreply.github.com>
2024-08-07 18:07:48 +0530

    Update MonthSummary.jsx

--- src/components/sections/MonthSummary.jsx
+++ src/components/sections/MonthSummary.jsx
@@ -17,7 +17,7 @@ const MonthSummary = () => {
 
 const todayTotalExpense = useMemo(() =>
         expenseList.reduce(function (accumulator, curValue) {
-            return accumulator + ((curValue.expenseType !== "salary" && curValue.expenseType !== "person" && Date(curValue.expenseDate) === Date()) ? Number(curValue.expenseAmount) : 0)
+            return accumulator + ((curValue.expenseType !== "salary" && curValue.expenseType !== "person" && Date(curValue.expenseDate) ===  Date().toLocaleDateString('en-GB').replace(/\//g, '-') ) ? Number(curValue.expenseAmount) : 0)
         }, 0)
         , [expenseList])
 
