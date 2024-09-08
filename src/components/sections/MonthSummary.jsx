fd2cb4818f5882bf6c1d93f7fa475f2d2c9d809a
Pavan Rayudu <122986277+pavanRayudu@users.noreply.github.com>
2024-09-08 16:19:19 +0530

    Update MonthSummary.jsx

--- src/components/sections/MonthSummary.jsx
+++ src/components/sections/MonthSummary.jsx
@@ -28,9 +28,9 @@ const MonthSummary = () => {
             {isLoading ? <Skeleton width={100} baseColor="#dbdbdb" highlightColor="gray" /> : <h2>This month</h2>}
             {isLoading ? <Skeleton count={2} baseColor="#dbdbdb" highlightColor="gray" /> : <div>
                 <p>You have earned  <span id='income'>Rs.{totalReceivedMoneyInCurrentMonth}
-                </span> and Spent <span id='spent'>Rs.{totalExpenseInCurrentMonth}</span> </p>
-                <p>Bal: <span id='balance'>Rs.{totalReceivedMoneyInCurrentMonth - totalExpenseInCurrentMonth}</span></p>*/}
-
+                </span> and Spent <span id='spent'>Rs.{totalExpenseInCurrentMonth}</span>, Balance:  
+{totalReceivedMoneyInCurrentMonth - totalExpenseInCurrentMonth}
+</p>
 </div>
 
 
